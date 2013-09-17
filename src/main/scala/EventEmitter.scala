d5afa7b3306459f480f040864a72f953624fd8f8
TakumiBaba <s09704tb@gmail.com>
2013-09-18 03:18:27 +0900

    emit 時に異なる引数を与えていたので修正　

--- src/main/scala/EventEmitter.scala
+++ src/main/scala/EventEmitter.scala
@@ -23,7 +23,6 @@ trait EventEmitter {
       "params" -> params,
       "id" -> id
     ))
-    println(events)
   }
   def on(typ:String, params:Map[String, Any], callback:(List[Any])=>Any){
     add_listener(typ, params, callback);
@@ -60,26 +59,27 @@ trait EventEmitter {
         if(params.get("once").getOrElse(false).asInstanceOf[Boolean]){
           events = events.filterNot(_ == event)
         }
-        listener(List(params))
+//        listener(List(params))
+        listener(data)
       }else if(eventType.matches("_")){
         var listener:(Any)=>Any = e.get("listener").get.asInstanceOf[(Any)=>Any];
         var params:Map[String, Any] = e.getOrElse("params", Map()).asInstanceOf[Map[String, Any]]
         if(params.get("once").getOrElse(false).asInstanceOf[Boolean]){
           events = events.filterNot(_ == event)
         }
-        listener(List(typ, params))
+//        listener(List(typ, params))
+        listener(data)
       }
 
     }
-    for(event <- events){
-      var e:Map[String, Any] = event.asInstanceOf[Map[String, Any]];
-      if(e.get(typ).isEmpty){
-        remove_listener(e.get("id").get)
-      }
-
-    }
+//    for(event <- events){
+//      var e:Map[String, Any] = event.asInstanceOf[Map[String, Any]];
+//      if(e.get(typ).isEmpty){
+//        remove_listener(e.get("id").get)
+//      }
+//    }
   }
-  def once(typ:String, callback:(List[Any])=>Unit) = {
+  def once(typ:String, callback:(List[Any])=>Any) = {
     add_listener(typ, Map("once" -> true), callback);
   }
 }
